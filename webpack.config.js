4ac9c713ad6764f6eed1f9f8cab89e6325a1e9ea
xx熊新 <xiong_x@ctrip.com>
2018-02-09 21:09:19 +0800

    init react-trip

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = {
+    title: 'react-trip',
+    filename: 'index.html',
+    template: 'template.html',
+    inject: true // true|body|head|false，四种值，默认为true,true和body相同,是将js注入到body结束标签前,head将打包的js文件放在head结束前,false是不注入，这时得要手工在html中加js
+}
+
+module.exports = {
+    entry: './src/main',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/, 
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(HtmlWebpackPluginConfig),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ], 
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        index: 'index.html',
+        host: 'localhost',
+        port: 6000,
+        hot: true,
+        compress: true,
+        inline: true
+    }
+};
